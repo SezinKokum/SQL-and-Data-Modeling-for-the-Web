63e15f41f270317ebef58f2cb0b66f116132b702
sezinkokum <sezinkokum@gmail.com>
2021-03-20 15:42:08 +0300

    Hello World with Flask App-Alternative approach to run a Flask app: using __main__

--- flask-hello-app.py
+++ flask-hello-app.py
@@ -11,3 +11,7 @@ def index():
 
 #For debug mode, you can run this app by writing the command below to terminal
 #FLASK_APP=flask-hello-app.py FLASK_DEBUG=true flask run
+
+#if you add the code which is below, you can run the app by writing the command: python flask-hello-app.py
+if __name__ == '__main__':
+  app.run()
