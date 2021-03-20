24242340260f3b32f1295d5c53a9885b2b41bb9e
sezinkokum <sezinkokum@gmail.com>
2021-03-20 15:35:32 +0300

    Hello World with Flask App-debug mode

--- flask-hello-app.py
+++ flask-hello-app.py
@@ -8,3 +8,6 @@ def index():
 
 #you can run this app by writing the command below to terminal
 #FLASK_APP=flask-hello-app.py flask run
+
+#For debug mode, you can run this app by writing the command below to terminal
+#FLASK_APP=flask-hello-app.py FLASK_DEBUG=true flask run
