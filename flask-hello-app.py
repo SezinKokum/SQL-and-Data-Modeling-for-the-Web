a34e609ade72ed5c9d2201c60b6566c3ab5e3138
sezinkokum <sezinkokum@gmail.com>
2021-03-20 19:51:12 +0300

    Hello World with Flask App-Creating a person class with db.Model

--- flask-hello-app.py
+++ flask-hello-app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:secret@localhost:5432/mydb'
 db = SQLAlchemy(app)
 
+class Person(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
 @app.route('/')
 def index():
     return 'Hello World!'
