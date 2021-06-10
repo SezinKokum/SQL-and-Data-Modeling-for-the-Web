2aecbf78f13d891cba1d8b2087b52d3bb940d05f
sezinkokum <sezinkokum@gmail.com>
2021-06-10 23:24:09 +0300

    Session-Getting User Data in Flask

---
+++ session.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:secret@localhost:5432/mydb'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
+
+@app.route('/todos/create', methods = ['GET', 'POST'])
+def create_todo():
+  description = request.get_json()['description']
+  todo = Todo(description=description)
+  db.session.add(todo)
+  db.session.commit()
+  return jsonify({
+    'description': todo.description
+  })
+
+
+@app.route('/')
+def index():
+  return render_template('session.html', data=Todo.query.all())
